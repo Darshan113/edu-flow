cbbf392a4bbfee02a0348a2ad7b6d932e9ff48a9
darshan.p <darshan.p@dosepack.com>
2025-09-18 18:40:40 +0530

    feat: add greeting and counter component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   let name  = 'darshan'
   return (
-    
     <>
-      Hello {name}, Greetings.
+      <h1>Hello EduFlow 🚀</h1>
+      <Greetings name='Darshan'/>
+      <div className="counter-container">
+        <Counter/>
+      </div>
+    </>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="counter-box">
+        {count}
+      </div>
+      <div className="counter-actions">
+        <button onClick={() => setCount(count+1)}>+</button>
+        <button onClick={() => setCount(count-1)}>-</button>
+      </div>
+    </>
+  )
+}
+
+function Greetings({name}) {
+  return (
+    <>
+      <p>Hello, {name}</p>
     </>
   )
 }
