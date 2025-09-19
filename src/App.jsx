e09c2f263d4a01165574e28a790db26d87c8026b
darshan.p <darshan.p@dosepack.com>
2025-09-19 14:05:30 +0530

    feat: style components and enhance state

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   let name  = 'darshan'
   return (
     <>
-      <h1>Hello EduFlow 🚀</h1>
+      <h1 className='text-2xl font-bold text-blue-600'>Hello EduFlow 🚀</h1>
       <Greetings name='Darshan'/>
       <div className="counter-container">
         <Counter/>
@@ -16,15 +16,21 @@ function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+
+  const increaseByTwo = () => {
+    setCount(prev => prev + 2)
+  }
   return (
     <>
       <div className="counter-box">
         {count}
       </div>
       <div className="counter-actions">
-        <button onClick={() => setCount(count+1)}>+</button>
+        <button className='bg-green-500 px-4 py-2 rounded hover:bg-green-600 transition' onClick={() => setCount(count+1)}>+</button>
         <button onClick={() => setCount(count-1)}>-</button>
+        <button className='bg-green-500 px-4 py-2 rounded hover:bg-green-600 transition' onClick={() => increaseByTwo()}>+</button>
       </div>
+      <Input />
     </>
   )
 }
@@ -33,6 +39,19 @@ function Greetings({name}) {
   return (
     <>
       <p>Hello, {name}</p>
+    </>
+  )
+}
+
+function Input() {
+  const [text,setText] = useState('');
+  const handleTextChange = (value) => {
+    setText(value);
+  }
+  return (
+    <>
+      <p>You have wrote -- {text}</p>
+      <input className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' type="text" value={text} onChange={(e) => handleTextChange(e.target.value)} />
     </>
   )
 }
